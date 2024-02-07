2481369e04e9e549839b6d46783a812affe4fe3a
shubhamsudrik <shubhams@ssktech.co.in>
2024-02-07 19:54:37 +0530

    commit

--- src/main/java/com/demo/demo/servicesImpl/EmpServicesImpl.java
+++ src/main/java/com/demo/demo/servicesImpl/EmpServicesImpl.java
@@ -1,5 +1,20 @@
 package com.demo.demo.servicesImpl;
 
-public class EmpServicesImpl {
+import java.util.List;
 
+import com.demo.demo.payload.EmpDto;
+import com.demo.demo.repository.EmpRepo;
+import com.demo.demo.services.EmpService;
+
+public class EmpServicesImpl implements EmpService {
+
+	private EmpRepo empRepo;
+	
+	@Override
+	public List<EmpDto> addEmpData(EmpDto empDto) {
+		
+		return empRepo.save(empDto);
+	}
+
+	
 }
